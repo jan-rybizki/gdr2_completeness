63a132372b9c7a3aed843a263861380e2c7cd7c8
Jan Rybizki <jan.rybizki@weyhausen.de>
2018-06-01 10:41:14 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='gdr2_completeness',
+      version='0.1',
+      description='calculating the completeness for specific GDR2 selection',
+      url='http://github.com/jan-rybizki/gdr2_completeness',
+      author='Jan Rybizki',
+      author_email='rybizki@mpia.de',
+      license='CC BY-SA 4.0',
+      packages=['gdr2_completeness'],
+      zip_safe=False)
+
